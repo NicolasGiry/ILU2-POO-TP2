fb931ae74e29d6e76d05b01efb54dbce77c79d73
NicolasGiry <nicolas.giryfr@gmail.com>
2023-10-12 09:11:22 +0200

    TP2-4 Prendre Etal

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,29 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("Je suis désolé "+ nomVendeur +" mais il faut être un habitant de notre village pour commercer ici.");
+		} else {
+			System.out.println("Bonjour "+ nomVendeur +", je vais regarder si je peux vous trouver un étal");
+			boolean etalDisponible = controlPrendreEtal.resteEtals();
+			if (!etalDisponible) {
+				System.out.println("Désolé.e "+ nomVendeur +" je n'ai plus détal qui ne soit pas déjà occupé.");
+			} else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait il me reste un étal pour vous !");
+		System.out.println("Il me faudrait quelques renseignements :");
+		System.out.println("Quel produit souhaitez-vous vendre ?");
+		String produit = scan.next();
+		System.out.println("Combien souhaitez-vous en vendre ?");
+		int nbProduits = scan.nextInt();
+		int numEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduits);
+		if (numEtal != -1) {
+			System.out.println("Le vendeur "+ nomVendeur +" s'est installé à l'étal n°"+ numEtal);
+		}
 	}
 }
