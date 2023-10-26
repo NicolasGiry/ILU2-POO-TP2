8c011571d4031d11f095df2dfb53314b0070446f
NicolasGiry <nicolas.giryfr@gmail.com>
2023-10-26 09:07:16 +0200

    TP2-6 Liberer Etal v2

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -16,7 +16,7 @@ public class BoundaryLibererEtal {
 		} else {
 			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
 			if (donneesEtal[0]=="true") {
-				System.out.println("Vous avez vendu "+ donneesEtal[2]+" sur "+donneesEtal[3]+ " "+donneesEtal[4]);
+				System.out.println("Vous avez vendu "+ donneesEtal[4]+" sur "+donneesEtal[3]+ " "+donneesEtal[2]);
 				System.out.println("Au revoir "+ nomVendeur+ ", passez une bonne journée.");
 			}
 		}
