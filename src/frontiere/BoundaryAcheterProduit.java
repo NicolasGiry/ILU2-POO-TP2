c160cfee671ad7d9f954ba3488f0a172c9abbdbe
NicolasGiry <nicolas.giryfr@gmail.com>
2023-10-26 09:44:21 +0200

    TP2-8 debut boundary

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -13,6 +13,25 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO Ã  completer
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Quel produit voulez-vous acheter .");
+		String produit = scanner.next();
+		String vendeurs[] = controlAcheterProduit.getVendeursProduit("produit");
+		if (vendeurs.length == 0) {
+			System.out.println("Désolé, personne ne vend ce produit au marché");
+		} else {
+			System.out.println("Chez quel commerçant souhaitez-vous acheter des fleurs ?");
+			for (int i=0; i< vendeurs.length; i++) {
+				System.out.println(i+1+" - "+vendeurs[i]);
+			}
+			int vendeur = scanner.nextInt() - 1;
+			System.out.println(nomAcheteur+" se déplace jusqu'à l'étal du vendeur "+vendeurs[vendeur]);
+			System.out.println("Bonjour "+nomAcheteur+".\nCombien de "+produit+" voulez-vous acheter ?");
+			int quantite = scanner.nextInt();
+			System.out.println(controlAcheterProduit.acheter(vendeurs[vendeur], produit, quantite));
+			
+		}
+		
+		
 	}
 }
